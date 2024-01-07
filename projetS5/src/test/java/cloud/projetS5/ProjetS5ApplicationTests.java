f7b52a323f38bfc2c1a324d0ac5e79e49324d93a
Antsa-Malala <malalanirina030304@gmail.com>
2024-01-07 22:41:43 +0300

    init

---
+++ projetS5/src/test/java/cloud/projetS5/ProjetS5ApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.projetS5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetS5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
